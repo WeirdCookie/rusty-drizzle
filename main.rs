a09e8e7f3191a57e869cbf9f95ea169632619353
WeirdCookie <olebeuscher@gmail.com>
2026-08-11 19:33:16 +0200

    added image crate

--- main.rs
+++ main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use std::io::Cursor;
+use image::ImageReader;
+
+fn main() -> image::ImageResult<()> {
+    let img = ImageReader::open("myimage.png")?.decode()?;
+    let mut bytes: Vec<u8> = Vec::new();
+    let img2 = ImageReader::new(Cursor::new(&bytes)).with_guessed_format()?.decode()?;
+    img.save("empty.jpg")?;
+    img2.write_to(&mut Cursor::new(&mut bytes), image::ImageFormat::Png)?;
+    Ok(())
+}
