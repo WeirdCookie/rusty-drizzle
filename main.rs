42ae783e0a2e977c5646b6a9577e4a91fabebb9f
WeirdCookie <olebeuscher@gmail.com>
2026-08-12 18:05:38 +0200

    added new variables

--- main.rs
+++ main.rs
@@ -5,10 +5,15 @@ fn main() {
     let width = 400;
     let height = 300;
     let mut img = ImageBuffer::new(width, height);
-
+    let numdrops: u32 = 25;
+    let angle:i32 = 15;
     
     let mut rng = rand::thread_rng();
 
+     for pixel in img.pixels_mut() {
+        *pixel = Rgb([30, 30, 40]); // Nachthimmel
+    }
+    
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         
         let r: u8 = rng.gen();
