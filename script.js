ed02b93dba031f1a51c81ad3c75c9c1e47c34740
WeirdCookie <olebeuscher@gmail.com>
2026-08-15 18:12:31 +0200

    added a loop that finally works

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ import init, { draw_noise } from './pkg/rusty_drizzle.js';
 
 const canvas = document.getElementById('noise-canvas');
 const ctx = canvas.getContext('2d');
-const refreshButton = document.getElementById('refresh-btn');
 
 
 function drawNoise() {
     draw_noise(ctx, canvas.width, canvas.height);
+    requestAnimationFrame(drawNoise);
 }
 
 
 async function start() {
     await init();
     drawNoise();
-    refreshButton.addEventListener('click', drawNoise);
 }
 
 
