8e5dd5ab2a4f9dd649c9c0af948784c59b3fa3f3
WeirdCookie <olebeuscher@gmail.com>
2026-08-15 15:19:36 +0200

    make the refresh button work

--- script.js
+++ script.js
@@ -4,11 +4,18 @@ import init, { draw_noise } from './pkg/rusty_drizzle.js';
 
 const canvas = document.getElementById('noise-canvas');
 const ctx = canvas.getContext('2d');
+const refreshButton = document.getElementById('refresh-btn');
+
+
+function drawNoise() {
+    draw_noise(ctx, canvas.width, canvas.height);
+}
 
 
 async function start() {
-    await init();                
-    draw_noise(ctx, 640, 480);   
+    await init();
+    drawNoise();
+    refreshButton.addEventListener('click', drawNoise);
 }
 
 
